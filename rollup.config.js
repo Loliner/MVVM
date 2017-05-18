64ca36cbb228bf679bdd4f641b6338b1060e541e
zhengjiu <zhengjiu@baidu.com>
2017-05-18 10:47:57 +0800

    mvvm prototype

---
+++ rollup.config.js
@@ -0,0 +1,20 @@
+import resolve from 'rollup-plugin-node-resolve';
+import babel from 'rollup-plugin-babel';
+
+export default {
+    entry: 'src/main.js',
+    format: 'iife',
+    moduleName: 'MVVM',
+    plugins: [
+        resolve({
+            // pass custom options to the resolve plugin
+            customResolveOptions: {
+                moduleDirectory: 'node_modules'
+            }
+        }),
+        babel({
+            exclude: 'node_modules/**' // only transpile our source code
+        })
+    ],
+    dest: 'dist/MVVM.js'
+}
---
+++ src/main.js
@@ -0,0 +1,56 @@
+class MVVM {
+    constructor(selector, options) {
+        this.el = window.document.querySelector(selector);
+        this.data = {};
+        this.bindings = {};
+        this.parseNode(this.el);
+
+        for (let key in options.data) {
+            this.data[key] = options.data[key];
+        }
+    }
+
+    parseNode(el) {
+        for (let i = 0; i < el.childNodes.length; i++) {
+            let node = el.childNodes[i];
+            // 节点
+            if (node.nodeType === 1) {
+                this.parseNode(node);
+                // 文本节点
+            } else if (node.nodeType === 3) {
+                let match = /{{\s*(\w+)\s*}}/i.exec(node.textContent);
+                if (match && match[1]) {
+                    let key = match[1];
+                    if (this.bindings[key]) {
+                        this.bindings[key].nodes.push(node);
+                    } else {
+                        this.createAccessor(key, node);
+                    }
+                }
+            }
+        }
+    }
+
+    createAccessor(key, node) {
+        let binding = {
+            value: '',
+            nodes: [node]
+        };
+        this.bindings[key] = binding;
+        Object.defineProperty(this.data, key, {
+            get: function (key) {
+                return binding.value;
+            },
+            set: function (newVal) {
+                binding.value = newVal;
+                let nodes = binding.nodes;
+                for (let i = 0; i < nodes.length; i++) {
+                    nodes[i].textContent = newVal;
+                }
+            }
+        });
+    }
+
+}
+
+export default MVVM;
